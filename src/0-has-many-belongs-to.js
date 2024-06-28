13df0c12ba1b6c6c1cefd932f34441fca1589419
roenc576 <roenc576@gmail.com>
2024-06-28 15:18:25 -0400

    Passed all 15 tests in has many / belongs to

--- src/0-has-many-belongs-to.js
+++ src/0-has-many-belongs-to.js
@@ -1,27 +1,52 @@
 const { getId } = require('./utils');
 
 class ToDoItem {
-  constructor(description, priorityLevel) {
+  constructor(description, priorityLevel, deadline) {
     this.id = getId();
     this.description = description;
     this.priorityLevel = priorityLevel;
+    this.deadline = deadline;
     this.isDone = false;
   }
-}
 
+  getItem() {
+    if (!this.isDone) {
+      console.log(`You still have to ${this.description}`);
+    }
+    return this;
+  }
+}
 
 class ToDoList {
   static #lists = [];
   #items = [];
+  #schedule = [];
+  owner = '';
 
-  constructor(name) {
+  constructor(name, owner) {
     this.id = getId();
     this.name = name;
+    this.owner = owner
     ToDoList.#lists.push(this);
   }
 
-  createItem(description, priorityLevel) {
-    const item = new ToDoItem(description, priorityLevel);
+  createItem(description, priorityLevel, deadline) {
+    const item = new ToDoItem(description, priorityLevel, deadline);
+
+    // Insert item into the schedule in the correct order
+    let inserted = false;
+    for (let i = 0; i < this.#schedule.length; i++) {
+      if (item.deadline < this.#schedule[i].deadline) {
+        this.#schedule.splice(i, 0, item);
+        inserted = true;
+        break;
+      }
+    }
+
+    if (!inserted) {
+      this.#schedule.push(item);
+    }
+
     this.#items.push(item);
     return item;
   }
@@ -31,18 +56,35 @@ class ToDoList {
   }
 
   getCompletedCount() {
-    return [...this.#items].filter(item => item.isDone === true).length;
+    return [...this.#items].filter(item => item.isDone).length;
+  }
+
+  printSchedule() {
+    let msg = `Hi hi :D Here's the order you should do these things in: `;
+
+    this.#schedule.forEach(item => {
+      msg += `\n- ${item.description} (Deadline in ${item.deadline} hours)`;
+    });
+
+    console.log(msg);
   }
 
   static list() {
     return [...this.#lists];
   }
 
-  static findBy() {
-
+  static findBy(listId) {
+    return [...this.#lists].find(list => list.id === listId);
   }
-
 }
+
+const myList = new ToDoList("rawr");
+myList.createItem('laundry', 8, 3);
+myList.createItem('homework', 10, 4);
+myList.createItem('groceries', 4, 1);
+myList.createItem('dog', 7, 1);
+myList.printSchedule();
+
 
 module.exports = {
   ToDoItem,
