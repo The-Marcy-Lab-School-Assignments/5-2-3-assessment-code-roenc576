138b769f96a0730d0a0dc3b9960fb26b6014516b
roenc576 <roenc576@gmail.com>
2024-06-28 16:07:11 -0400

    Passed 5 tests in second suite

--- src/1-inheritance-polymorphism.js
+++ src/1-inheritance-polymorphism.js
@@ -1,10 +1,37 @@
 class Phone {
+  contacts = [];
+
+  constructor(phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+  makeCall() {
+
+  }
+
+  addContact(contact) {
+    const parsedNum = +contact.phoneNumber;
+    if (!contact.name || !contact.phoneNumber) {
+      return `Invalid`;
+    } else if (contact.phoneNumber.length !== 10 || typeof parsedNum !== 'number') {
+      return `Invalid`;
+    }
+    this.contacts.push(contact);
+    return `${contact.name} added.`
+  }
+
+  removeContact() {
+
+  }
 
 }
 
 class AppleIPhone {
 
 }
+
+const myPhone = new Phone('3448731233');
+console.log(myPhone);
 
 module.exports = {
   Phone,
