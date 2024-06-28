19f8df9c9cf2ba02b49cc8be732fcaf7c44c620f
roenc576 <roenc576@gmail.com>
2024-06-28 14:00:26 -0400

    Passed 10 tests" "

--- src/0-has-many-belongs-to.js
+++ src/0-has-many-belongs-to.js
@@ -11,25 +11,34 @@ class ToDoItem {
 
 
 class ToDoList {
-
+  static #lists = [];
   #items = [];
 
   constructor(name) {
     this.id = getId();
     this.name = name;
+    ToDoList.#lists.push(this);
   }
 
   createItem(description, priorityLevel) {
     const item = new ToDoItem(description, priorityLevel);
-    [...this.#items].push(item);
+    this.#items.push(item);
     return item;
   }
 
   getItems() {
-
+    return [...this.#items];
   }
 
   getCompletedCount() {
+    return [...this.#items].filter(item => item.isDone === true).length;
+  }
+
+  static list() {
+    return [...this.#lists];
+  }
+
+  static findBy() {
 
   }
 
